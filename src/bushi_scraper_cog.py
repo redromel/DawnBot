2d8c00eb84d21e7d6fc8aa51b012744b5a47eab7
Alec Espino <espino.alec@gmail.com>
2025-09-18 22:50:32 -0400

    Added get_decklist, useful for sims

--- src/bushi_scraper_cog.py
+++ src/bushi_scraper_cog.py
@@ -30,6 +30,24 @@ class BushiScraperCog(commands.Cog):
             f'EN Decklink successfully created:  {json_response["url"]}')
         return
 
+    @commands.slash_command(name="getdecklist", description="Get Decklist for Deck to use for the https://sveclient.vercel.app/ sim")
+    async def get_decklist(self, ctx, url: str):
+        await ctx.defer()
+        body = {'url': url}
+        response = requests.post(
+            f"{API_BASE_URL}/decks/simDecklist", json=body)
+
+        if response.status_code >= 400:
+            await ctx.followup.send(
+                f'There was an error with the request: "{json_response["error"]}" ', ephemeral=True)
+            return
+
+        json_response = response.json()
+        deck = json_response["deck"]
+        formatted_deck = f"```{deck}```"
+        await ctx.followup.send(f"Decklist for https://sveclient.vercel.app/ Created:\n{formatted_deck}")
+        return
+
 
 def setup(bot):
     bot.add_cog(BushiScraperCog(bot))
